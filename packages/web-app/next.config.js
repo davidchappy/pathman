e9a904a5f0af6911dc725d09f54cd650c2657a00
davidchappy <davidchappy@gmail.com>
2024-02-25 23:47:25 -0600

    Setup for github pages

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@pathman/components", "@pathman/core"],
+  output: "export",
+  reactStrictMode: true,
+  basePath: "pathman"
 }
 
 module.exports = nextConfig
